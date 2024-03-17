7f5817c8be1f1ba7ffcee2f97b8a0384de6b441f
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2024-03-17 21:38:37 +1100

    the react root component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+// In a React project, the root component is the top-level component that serves as the entry point of the entire React application. 
+// It is where the React component tree is mounted onto the DOM.
+// App -> index.js -> public/index.html
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
